f8212122e18a5dac87ae6bc5f1a6dbd665a4b881
equp <keerthy.mohankumar@auspost.com.au>
2018-12-07 15:02:11 +1100

    ++Files

---
+++ AutomationTesting/src/test/java/com/framework/utility/exceptions/InvalidDriverTypeSelectedException.java
@@ -0,0 +1,8 @@
+package com.framework.utility.exceptions;
+
+public class InvalidDriverTypeSelectedException extends Exception {
+
+    public InvalidDriverTypeSelectedException(String message) {
+        super(message);
+    }
+}
